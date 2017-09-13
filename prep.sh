f9fa841cf43ab6b3aa3e848bd91ef8a3f1524b06
LuckyRepo <derrick@klabouch.com>
2017-09-13 00:08:14 -0400

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,8 +1,12 @@
 #/bin/bash
 	clear
-	echo -e '\e[104mgappsbuilder by LuckyRepo v0.0.1\e[0m'
+	echo -e '\e[104mgappsbuilder by LuckyRepo v0.0.2\e[0m'
 	echo -e '\e[91mThis build still contains test code. Not for daily use. Devs and testers Only.\e[0m'
-# Ask if they want to set SDK path
+		arm=$(./download_sources.sh [--shallow] [arm])
+		arm64=$(./download_sources.sh [--shallow] [arm64])
+		x86=$(./download_sources.sh [--shallow] [x86])
+		x8664=$(./download_sources.sh [--shallow] [x86_64])
+# Ask if they want to set up
 	echo -e '\e[32mDo you want to set the SDK path?\e[0m'
 	select yn in "Yes" "No"; do
     		case $yn in
@@ -10,12 +14,26 @@
         		No ) echo -e '\e[96mSkipping\e[0m'; break;;
     		esac
 	done
-# Assign the build path on launch
+# 
+	echo -e '\e[32mspecify architecture\e[0m'
+	select sa in "arm" "arm64" "x86" "x86_64"; do
+    		case $sa in
+        		arm ) echo -e '\e[96msync arm submodules\e[0m'; echo $arm >> archpath.txt && echo arm >> arch.txt && break;;
+        		arm64 ) echo -e '\e[96msync arm64 submodules\e[0m'; $arm64 >> archpath.txt && echo arm64 >> arch.txt && break;;
+			x86 ) echo -e '\e[96msync x86 submodules\e[0m'; $x86 >> archpath.txt && echo x86 >> arch.txt &&  break;;
+			x86_64 ) echo -e '\e[96msync x86_64 submodules\e[0m'; $x8664 >> archpath.txt && echo X86_64 >> arch.txt &&  break;;
+    		esac
+	done	
+# Assign the build path
 	line=$(head -n 1 sdkpath.txt)
-	cd $line# 
-Set PATH for all needed commands
+	cd $line
+# Assign arch sync
+	arch=$(head -n 1 archpath.txt)
+# Assign arch type sync
+	archt=$(head -n 1 arch.txt)
+#Set PATH for all needed commands
 	PATH=~/bin:$PATH
-  export ANDROID_HOME=~/build/gapps/opengapps/android-sdk-linux
+  export ANDROID_HOME=$line
   export PATH=$ANDROID_HOME/tools:$PATH
   export PATH=$ANDROID_HOME/platform-tools:$PATH
 	echo -e '\e[32mChanged BIN Path to current folder to allow for Repo commands\e[0m'
@@ -25,32 +43,17 @@ Set PATH for all needed commands
 		export USE_CCACHE=1
 		export CCACHE_COMPRESS=1
     	echo -e '\e[32mSet CCACHE to 100G, set compression and set use to 1\e[0m'
-# Set Jack server to use 6g to avoid build errors
-		cd $PWD/prebuilts/sdk/tools
-		./jack-admin stop-server
-		export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx6g"
-		./jack-admin start-server
-            echo -e '\e[32mSet Jack Server to -Xmx6g\e[0m'
-    cd $OLDPWD
 # Ask if they want to sync in yes or no
-	echo -e '\e[32mDo you wish to sync the repo?\e[0m'
+	echo -e '\e[32mDo you wish to sync?\e[0m'
 	select yn in "Yes" "No"; do
     		case $yn in
-        		Yes ) echo -e '\e[96mStarting Sync\e[0m'; repo sync --force-sync && break;;
+        		Yes ) echo -e '\e[96mStarting Sync\e[0m'; $arch && break;;
         		No ) echo -e '\e[96mSkipping Sync\e[0m'; break;;
     		esac
 	done
 # Start Build
 	    echo -e '\e[32mMarking start of build\e[0m'
             sleep 1
-            source build/envsetup.sh
-# Ask if they want lunch or brunch
-	echo -e '\e[32mLunch or Brunch?\e[0m'
-	select lb in "Lunch" "Brunch"; do
-    		case $lb in
-        		Lunch ) echo -e '\e[96mUsing Lunch\e[0m'; lunch && break;;
-        		Brunch ) echo -e '\e[96mUsing Brunch\e[0m'; brunch && break;;
-    		esac
-	done
+		make $archt
 	    echo -e '\e[91mTurning controls back to you.\e[0m'
             exit
